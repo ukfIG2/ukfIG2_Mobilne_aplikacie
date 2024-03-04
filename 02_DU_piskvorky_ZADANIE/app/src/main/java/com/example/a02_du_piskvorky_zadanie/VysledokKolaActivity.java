820b405dea8d1017a3b84a0b913d9b100b556200
ukfIG2 <ivan.gabris2@student.ukf.sk>
2024-03-04 17:35:54 +0100

    Piskvorky asi hotovo

--- 02_DU_piskvorky_ZADANIE/app/src/main/java/com/example/a02_du_piskvorky_zadanie/VysledokKolaActivity.java
+++ 02_DU_piskvorky_ZADANIE/app/src/main/java/com/example/a02_du_piskvorky_zadanie/VysledokKolaActivity.java
@@ -3,17 +3,27 @@ package com.example.a02_du_piskvorky_zadanie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class VysledokKolaActivity extends AppCompatActivity {
     private TextView textViewVysledok;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vysledok_kola);
         textViewVysledok = findViewById(R.id.tv5);
         textViewVysledok.setText("Vyhral hrac: " + getIntent().getStringExtra("winner"));
+        button = findViewById(R.id.bt5);
+        button.setOnClickListener(e -> reset());
     }
+
+    private void reset() {
+        //I want to return to previus intent
+        finish();
+    }
+
 }
 
 //Q:How to check data type of variable in Java give me example. String a = "Hello";
