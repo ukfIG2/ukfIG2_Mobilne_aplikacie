144534b810bcdb85d58840f813dd85b99b876793
ukfIG2 <ivan.gabris2@student.ukf.sk>
2024-04-20 23:29:52 +0200

    nieco

---
+++ 0701_BroadcostReceiver/app/src/main/java/com/example/a0701_broadcostreceiver/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.a0701_broadcostreceiver;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    MyReceiver mojRec = new MyReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mojRec, new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mojRec);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
+}
---
+++ 0701_BroadcostReceiver/app/src/main/java/com/example/a0701_broadcostreceiver/MyReceiver.java
@@ -0,0 +1,13 @@
+package com.example.a0701_broadcostreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "zmena air plainu", Toast.LENGTH_SHORT).show();
+    }
+}
